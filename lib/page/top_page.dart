59d2b3e383c91442856499c1a358d4798dc65870
Kyosuke Takayama <loiseau@gmail.com>
2022-02-23 11:37:02 +0900

    feat: add RuleListWidget

--- lib/page/top_page.dart
+++ lib/page/top_page.dart
@@ -15,10 +15,29 @@ class TopPage extends StatelessWidget {
                 icon: const Icon(Icons.add))
           ],
         ),
-        body: const Center(child: Text('ルールを一覧表示するよ!')),
+        body: const RuleListWidget(),
         floatingActionButton: FloatingActionButton(
           onPressed: () => print('ルール追加画面に遷移するよ'),
           child: const Icon(Icons.add),
         ));
+  }
+}
+
+class RuleListWidget extends StatelessWidget {
+  const RuleListWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> rules = [
+      'ビールが欲しくなったら => 炭酸水を飲む',
+      '食べたくなったら => ナッツを食べる',
+      'タバコが吸いたくなったら => ニコレスを吸う',
+    ];
+
+    return ListView.builder(
+        itemCount: rules.length,
+        itemBuilder: (context, index) {
+          return Text(rules[index]);
+        });
   }
 }
