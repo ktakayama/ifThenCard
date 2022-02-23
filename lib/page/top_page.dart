9cfcec75c4571f6f1e66f4118520c94319ff6c26
Kyosuke Takayama <loiseau@gmail.com>
2022-02-23 11:28:51 +0900

    feat: separate top page from main.dart

---
+++ lib/page/top_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('CARD LIST'),
+          actions: [
+            Tooltip(message: 'アイコンボタンをホバーした時に表示されるテキスト'),
+            IconButton(
+                onPressed: () => print('ルール追加画面に遷移するよ'), icon: Icon(Icons.add))
+          ],
+        ),
+        body: Center(child: Text('ルールを一覧表示するよ!')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('ルール追加画面に遷移するよ'),
+          child: const Icon(Icons.add),
+        ));
+  }
+}
